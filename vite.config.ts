c80eb331d91aa522652d7430e1f06920c22d9805
gotaker <gotaker12@gmail.com>
2025-08-13 16:38:33 -0400

    still fixing vite eissue

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
+
+
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: '/',                                // <-- force root base in dev
+  server: { host: true, port: 5173, strictPort: true, hmr: { clientPort: 5173 } },
+  preview: { host: true, port: 5173, strictPort: true } // keep preview on 5173 to avoid confusion
+}))
+
+
+
+
+
+
 // Locally: leave VITE_BASE unset (defaults to '/')
 // GH Pages: set VITE_BASE=/ITRM_App/
-const base = process.env.VITE_BASE || '/'
+//const base = process.env.VITE_BASE || '/'
 
-export default defineConfig({
-  plugins: [react()],
-  base,
-  server: { host: true, port: 5173, strictPort: true, hmr: { clientPort: 5173 } },
-  preview: { host: true, port: 5173, strictPort: true }
-})
+//export default defineConfig({
+  //plugins: [react()],
+  //base,
+  //server: { host: true, port: 5173, strictPort: true, hmr: { clientPort: 5173 } },
+ // preview: { host: true, port: 5173, strictPort: true }
+//})
